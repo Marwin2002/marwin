eea1ea45f951698b594f44020df379db75bb39a1
Marwin2002 <musicbotid@gmail.com>
2025-01-01 17:17:18 +0530

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -10,74 +10,48 @@ as you want or you can collabe if you have new ideas.
 """
 
 
-import uvloop
-import asyncio
-import threading
+import sys
 
-uvloop.install()
-
-from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
-from pyrogram import Client, idle
-from pyrogram.enums import ChatMemberStatus
-
+from pyrogram import Client
 import config
 from ..logging import LOGGER
+from pyrogram.enums import ChatMemberStatus
 
-
-
-
-@app.route("/")
-def home():
-    return "Bot is running"
-
-
-def run():
-    app.run(host="0.0.0.0", port=8000, debug=False)
 
 class AlexaBot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="AviaxMusic",
+            "MusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
+            sleep_threshold=180,
+            max_concurrent_transmissions=4,
+            workers=50,
         )
+        LOGGER(__name__).info(f"Starting Bot...")
 
     async def start(self):
         await super().start()
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
-
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        self.mention = get_me.mention
         try:
             await self.send_message(
-                chat_id=config.LOG_GROUP_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                config.LOG_GROUP_ID, "» ᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ, ᴡᴀɪᴛɪɴɢ ғᴏʀ ᴀssɪsᴛᴀɴᴛ..."
             )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
+        except:
             LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            exit()
-
+            sys.exit()
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
-            )
-            exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
-
-    async def stop(self):
-        await super().stop()
+            LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
+            sys.exit()
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
